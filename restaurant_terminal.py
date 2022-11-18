3cd8a0c1b54c69428b596247c1e5691e1989c2b3
Tarun Kukreja <tkukreja@horizon.csueastbay.edu>
2022-11-17 21:31:31 -0800

    code till inserting into orderinfo table added

--- restaurant_terminal.py
+++ restaurant_terminal.py
@@ -1,7 +1,47 @@
+import database_operations
+from datetime import datetime, timedelta
+
+'''
+The following function gets the datetime from the restaurant and sends it to the customer
+'''
+# def communicateDateTime(dateTimeFromRestaurant):
+#     return dateTimeFromRestaurant
+
+
+def registerOrderInfo(orderId, restaurantId, totalPayment, readyDateTime):
+    connObj = database_operations.connectToDatabase()
+    database_operations.addOrderInfo(connObj, totalPayment, readyDateTime, orderId, restaurantId)
+    connObj.close()
+
+def lastOrderInfo():
+    connObj = database_operations.connectToDatabase()
+    orderInfoList = database_operations.getLastOrderInfo(connObj)
+
+    totalPrice = 0
+    # [(3, 1, 1000, 5), (3, 2, 1000, 4)]
+    print("Order ID       ItemName          Quantity")
+    for itemTuple in orderInfoList:
+        itemName = database_operations.getItemNameFromItemId(connObj, itemTuple[1])
+        validAndItemPrice = database_operations.isValidItem(connObj, itemTuple[1], itemTuple[2])
+        totalPrice += validAndItemPrice[1] * itemTuple[3]
+        print(str(itemTuple[0])+'            '+str(itemName) + '          '+str(itemTuple[3]))
+    totalPayment = 0.1 * totalPrice + totalPrice
+    print("Total payment: ", totalPayment)
+    connObj.close()
+    return (orderInfoList[0][0], orderInfoList[0][2], totalPayment)
 
 if __name__ == '__main__':
     # after the order is placed -> entry is done in the Orders table
     # restaurant will see the order ->
     # restaurant will enter the preparation time for the order - for now
     #
+    print("New Order\n")
+    orderId, restaurantId, totalPayment = lastOrderInfo()
+    readyDateTime = input("\nPlease enter the ready date(YYYY-MM-DD) and time(hh:mm:ss) for the order separated by space\n")
+
+    # insert into orderinfo values (1, 0, '2022-01-01 22:23:24', 34, 0, '2022-01-01 22:23:24', 1000)
+    # readyDateTimeObj = datetime.strptime(readyDateTime, '%Y-%m-%d %H:%M:%S')
+    registerOrderInfo(orderId, restaurantId, totalPayment, readyDateTime)
+    # communicateDateTime(readyDateTime)
+    # insert the order into Orders Info with ready Time, order Id, restaurant id
 
